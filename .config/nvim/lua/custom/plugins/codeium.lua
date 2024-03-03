0fbbc7281c080031f1d49bbca7e227692d517f67
Mohamed Ibraheem <mohamedibraheem509@gmail.com>
2024-03-03 23:17:08 +0200

    nvim: add some keybinds to codeium

--- .config/nvim/lua/custom/plugins/codeium.lua
+++ .config/nvim/lua/custom/plugins/codeium.lua
@@ -1,4 +1,10 @@
 return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
+  config = function()
+    vim.keymap.set('i', '<C-\\>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true, silent = true })
+    vim.keymap.set('n', '<leader>ct', vim.cmd.CodeiumToggle, { desc = '[c]odeium [t]oggle' })
+  end,
 }
